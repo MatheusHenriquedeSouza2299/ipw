0261c96096fccb66f583022784ef24382a8101e6
MatheusHenriquedeSouza <48722541+MatheusHenriquedeSouza2299@users.noreply.github.com>
2020-05-12 20:41:00 -0300

    Add files via upload

---
+++ formularioCadastroUsuario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Formulario Cadastro Usuario </title>
+	</head>
+	<body>
+		<form class="" action="controlerUsuario.php" method="post">
+			<label for="">Nome:</label>
+			<input type="text" name="nome" value=""><br>
+			<label for="">Email:</label>
+			<input type="email" name="email" value=""><br>
+			<label for="">Login:</label>
+			<input type="text" name="login" value=""><br>
+			<label for="">Senha:</label>
+			<input type="Password" name="senha" value=""><br>
+			<input type="submit" name="" value="CADASTRAR">
+		</form>
+		</body>
+		</html>
